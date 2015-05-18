7efa8b86074b3f0b3e92c7b508e92088149fdb08
Matheus Frozzi Alberton <matheusfrozzi@MacBook-de-Frozzi.local>
2015-05-18 13:37:27 -0300

    watch kit files

---
+++ PoopCheck WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,53 @@
+//
+//  InterfaceController.swift
+//  PoopCheck WatchKit Extension
+//
+//  Created by Matheus Frozzi Alberton on 14/05/15.
+//  Copyright (c) 2015 BEPiD. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import poopCheckWatchKit
+
+class InterfaceController: WKInterfaceController {
+
+    @IBOutlet weak var poopButton: WKInterfaceButton!
+    @IBOutlet weak var textLabel: WKInterfaceLabel!
+    
+    let poop = poopControl()
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+//        poopButton.
+//        poopButton.layer.cornerRadius = poopButton.frame.size.width / 2;
+//        poopButton.clipsToBounds = true;
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    @IBAction func makePoop() {
+        println("clicou")
+        var request = ["request": "pushEmoji"]
+        
+        WKInterfaceController.openParentApplication(request, reply: { (replyFromParent, error) -> Void in
+            if(error != nil) {
+                println("there was an error receiving a reply")
+            } else {
+                self.textLabel.setText("You pooped today")
+                println("Ok")
+                println(replyFromParent)
+            }
+        })
+    }
+}
